c6a07e75041b00c094d90ebed201423b560e3e1e
aegis1920 <aegis1920@gmail.com>
2019-12-09 16:59:20 +0900

    feat : init

---
+++ src/test/java/com/howaboutliving/HowaboutlivingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howaboutliving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowaboutlivingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
