b805cab8ce2fb14497ff1b3f9467d60959f3c507
aegis1920 <aegis1920@gmail.com>
2019-12-10 09:06:41 +0900

    feat : init package and folder

---
+++ src/main/java/com/howaboutliving/dao/PublicDataEnvironmentDao.java
@@ -0,0 +1,5 @@
+package com.howaboutliving.dao;
+
+public class PublicDataEnvironmentDao {
+
+}
---
+++ src/main/java/com/howaboutliving/service/PublicDataEnvironmentService.java
@@ -0,0 +1,5 @@
+package com.howaboutliving.service;
+
+public class PublicDataEnvironmentService {
+
+}
