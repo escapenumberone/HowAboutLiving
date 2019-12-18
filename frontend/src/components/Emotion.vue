e4c4c2c8f4b86c21ba0631151e042679498f91a9
aegis1920 <aegis1920@gmail.com>
2019-12-18 17:27:40 +0900

    Fix : emotion file

--- frontend/src/components/Emotion.vue
+++ frontend/src/components/Emotion.vue
@@ -1,8 +1,7 @@
 <template>
     <div>
-        <h1>hiru</h1>
-        <h1> {{emotion}}</h1>
-        <img v-if="emotion === 'Smile'" src="../../static/img/smile.png">
+        <h1>여긴 살기 좋다우!!</h1>
+        <img v-if="emotion === 'smile'" src="../../static/img/smile.png">
         <img v-else-if="emotion === 'impassivity'" src="../../static/img/impassivity.png">
         <img v-else-if="emotion === 'angry'" src="../../static/img/angry.png">
         <img v-else-if="emotion === 'vomit'" src="../../static/img/vomit.png">
@@ -15,7 +14,7 @@ export default {
     name: 'Emotion',
     data() {
         return {
-            emotion : "vomit"
+            emotion : "smile"
         }
     }
 }
--- frontend/src/components/TopBar.vue
+++ frontend/src/components/TopBar.vue
@@ -1,8 +1,8 @@
 <template>
               <!-- topbar -->
-           <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-            <h1 class="display-4">Display 4</h1>
-          </nav>
+        <nav class="navbar-light bg-white topbar mb-4 static-top shadow text-center">
+            <h1 class="display-4">{{currentLocation}}</h1>
+        </nav>
 
 </template>
 
@@ -10,13 +10,18 @@
 
 export default {
     name: 'TopBar',
+    props: {
+        currentLocation: String
+    },
     components: {
-
+        
     },
     data(){
         return {
+        }
+    },
+    methods: {
 
-        };
     }
 }
 </script>
