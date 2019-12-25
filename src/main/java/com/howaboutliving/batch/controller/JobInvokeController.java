c88f03d49611174a06ae1657f35dd0064695dcd3
juregon <juregon2@naver.com>
2019-12-25 20:05:53 +0900

    정리

--- src/main/java/com/howaboutliving/batch/controller/JobInvokeController.java
+++ src/main/java/com/howaboutliving/batch/controller/JobInvokeController.java
@@ -25,7 +25,7 @@ public class JobInvokeController {
 	@Autowired
 	Job environmentDailyAvgJob;
 
-	@Scheduled(cron = "0 30 * * * *")  // 매시 30분 0초에 실행(1시간 주기로 업데이트)
+//	@Scheduled(cron = "0 30 * * * *")  // 매시 30분 0초에 실행(1시간 주기로 업데이트)
 	public void publicDataEnvironmentBatch() {
 		System.out.println("환경 데이터 적재 배치 시작");
 		JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
@@ -37,7 +37,7 @@ public class JobInvokeController {
 		}
 	}
 	
-	@Scheduled(cron = "0 0 2 * * *") // 매일 새벽 2시에 실행 (1일 주기로 업데이트)
+//	@Scheduled(cron = "0 0 2 * * *") // 매일 새벽 2시에 실행 (1일 주기로 업데이트)
 	public void insertOneEnvironmentDailyAvgBatch() {
 		System.out.println("환경 일평균 데이터 적재 배치 시작");
 		JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
@@ -49,7 +49,7 @@ public class JobInvokeController {
 		}
 	}
 
-	@Scheduled(cron = "0 0 0 * * *")  // 매일 0시 0분 0초(자정)에 실행(1일 주기로 업데이트)
+//	@Scheduled(cron = "0 0 0 * * *")  // 매일 0시 0분 0초(자정)에 실행(1일 주기로 업데이트)
 	public void publicDataDisasterBatch() {
 		System.out.println("재해 데이터 적재 배치 시작");
 		JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
